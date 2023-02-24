3f39c4f2761e95733d396706018a363db44907d2
Rob Benton <rob.benton7@gmail.com>
2023-02-24 09:50:08 -0600

    Reader test works

--- FileBasics.Test/FixedLengthReaderTest.cs
+++ FileBasics.Test/FixedLengthReaderTest.cs
@@ -1,28 +1,39 @@
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging.Console;
 using FileReaders;
 
 namespace FileBasics.Test;
 
 public class FixedLengthReaderTest
 {
-    private FixedLengthReader fixedLengthReader;
+    private ILoggerFactory? loggerFactory;
 
-    public FixedLengthReaderTest()
+    [SetUp]
+    public void SetUp()
     {
-        var serviceProvider = new ServiceCollection()
-            .AddSingleton<FixedLengthReader>()
-            .AddLogging()
-            .BuildServiceProvider();
-
-        fixedLengthReader = serviceProvider.GetService<FixedLengthReader>()
-            ?? throw new ArgumentNullException(nameof(fixedLengthReader));
+        loggerFactory = LoggerFactory.Create(builder =>
+        {
+            builder.AddSimpleConsole(opts =>
+            {
+                opts.ColorBehavior = LoggerColorBehavior.Disabled;
+            });
+        });
     }
+
+    [TearDown]
+    public void TearDown()
+    {
+        if (loggerFactory is not null)
+            loggerFactory.Dispose();
+    }
+
+    private FixedLengthReader createFixedLengthReader() =>
+        new FixedLengthReader(loggerFactory!.CreateLogger<FixedLengthReader>());
 
     [TestCase("IdName  AmountWhenCreated\n0 name  00.0000001-01-01 ")]
     public void TestReadDataMatches(string record)
     {
+        var fixedLengthReader = createFixedLengthReader();
         var reader = new StringReader(record);
 
         fixedLengthReader.readAndPrint(reader);
