b862ce6bf96c589250fb66a70cd43369de93ba5d
Rob Benton <rob.benton7@gmail.com>
2023-02-24 12:17:44 -0600

    Added test for event

--- FileBasics.Test/FixedLengthReaderTest.cs
+++ FileBasics.Test/FixedLengthReaderTest.cs
@@ -39,4 +39,23 @@ public class FixedLengthReaderTest
 
         fixedLengthReader.readAndPrint(reader);
     }
+
+    [TestCase("IdName  AmountWhenCreated\n0 name  00.0000001-01-01 ")]
+    public void TestEventOnRead(string record)
+    {
+        var fixedLengthReader = createFixedLengthReader();
+        var reader = new StringReader(record);
+
+        var layout = new FixedLayout();
+
+        fixedLengthReader.OnRead += rec =>
+            layout = rec;
+
+        fixedLengthReader.readAndPrint(reader);
+
+        Assert.That(layout.Id, Is.EqualTo(0));
+        Assert.That(layout.Name, Is.EqualTo("name"));
+        Assert.That(layout.Amount, Is.EqualTo(0));
+        Assert.That(layout.WhenCreated, Is.EqualTo(new DateTime(1, 1, 1)));
+    }
 }
