83e54cfc8cbcc1bfcccc9d972a87ee20fa04f6fe
Rob Benton <rob.benton7@gmail.com>
2023-03-06 08:54:49 -0600

    Started EF Core project

---
+++ EntityFrameworkBasics/Program.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.Hosting;
+
+using IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(ConfigureServices)
+    .Build();
+
+void ConfigureServices(IServiceCollection services)
+{
+}
