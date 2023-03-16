32735bb297bfa79789340377f56708503c1e0a7d
Rob Benton <rob.benton7@gmail.com>
2023-03-16 08:24:05 -0500

    Refactored migration extension methods

--- EntityFrameworkBasics/Migrations/20230314201640_InitialCreate.cs
+++ EntityFrameworkBasics/Migrations/20230314201640_InitialCreate.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
+using Microsoft.EntityFrameworkCore.Migrations.Operations.Builders;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 #nullable disable
@@ -57,5 +59,34 @@ namespace EntityFrameworkBasics.Migrations
 
             migrationBuilder.DropSetUpdateFunction();
         }
+    }
+
+    public static class Extensions
+    {
+        public static OperationBuilder<SqlOperation> CreateSetUpdateFunction(this MigrationBuilder migrationBuilder)
+            =>
+            migrationBuilder.Sql(
+                """
+                create function set_update() returns trigger as
+                $$
+                begin
+                    new.updated = now();
+                    return new;
+                end;
+                $$ language plpgsql;
+                """
+            );
+
+        public static OperationBuilder<SqlOperation> DropSetUpdateFunction(this MigrationBuilder migrationBuilder)
+            => migrationBuilder.Sql("drop function set_update();");
+
+        public static OperationBuilder<SqlOperation> AddSetUpdateTrigger(this MigrationBuilder migrationBuilder, string table)
+            =>
+            migrationBuilder.Sql(
+                $"""
+                create trigger {table}_setupdate_trg before insert or update on {table}
+                    for each row execute procedure set_update();
+                """
+            );
     }
 }
